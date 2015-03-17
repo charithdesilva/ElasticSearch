456eb05fccf1b8b95a45e9b7ada2f71c4065d029
Charith De Silva <cdesilva@DEAMELHQGR622.services.esriaustralia.com.au>
2015-03-18 09:25:24 +1100

    init

---
+++ ElasticSearch/src/main/java/chades/elasticsearch/elasticsearch/App.java
@@ -0,0 +1,21 @@
+package chades.elasticsearch.elasticsearch;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+import static org.elasticsearch.node.NodeBuilder.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	Node node = nodeBuilder().clusterName("chades1").node();
+	Client client = node.client();
+	// on shutdown
+
+	//node.close();
+    }
+}
